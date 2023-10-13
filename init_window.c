82ac5aae664c6adb66a72d3aadf7b79a0ed8315c
Luca Masetti <lmasetti@c2r4p8.42firenze.it>
2023-10-13 12:50:56 +0200

    fixed textures loading errors

--- init_window.c
+++ init_window.c
@@ -6,7 +6,7 @@
 /*   By: lmasetti <lmasetti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 15:01:20 by lmasetti          #+#    #+#             */
-/*   Updated: 2023/10/13 11:05:39 by lmasetti         ###   ########.fr       */
+/*   Updated: 2023/10/13 12:47:40 by lmasetti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,17 @@ bool	load_img(void *mlx_ptr, t_image *texture, char **path)
 
 	texture->img_ptr = mlx_xpm_file_to_image(mlx_ptr, path[1], &size, &size);
 	if (!texture->img_ptr)
-		return (write(1, "Error in loading image\n", 24));
+	{
+		write(1, "Error in loading image\n", 24);
+		return (false);
+	}
 	texture->addr = mlx_get_data_addr(texture->img_ptr,
 			&texture->bits_per_pixel, &texture->line_length, &texture->endian);
 	if (!texture->addr)
-		return (write(1, "Error in addres image\n", 23));
+	{
+		write(1, "Error in addres image\n", 23);
+		return (false);
+	}
 	return (true);
 }
 
